6f175056d78a23ea22cd321b621b0b0533c7ba6c
hojatv <hojat.vaheb@gmail.com>
2017-05-24 23:54:44 +0800

    TransactionSupport

--- src/main/java/org/learning/abshar/Application.java
+++ src/main/java/org/learning/abshar/Application.java
@@ -2,11 +2,13 @@ package org.learning.abshar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Spring boot main Application
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] args)  throws Exception{
         SpringApplication.run(Application.class,args);
--- src/main/java/org/learning/abshar/ws/service/GreetingServiceBean.java
+++ src/main/java/org/learning/abshar/ws/service/GreetingServiceBean.java
@@ -4,6 +4,8 @@ import org.learning.abshar.ws.model.Greeting;
 import org.learning.abshar.ws.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
 import java.util.Collection;
@@ -11,11 +13,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS,
+        readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
 
     @Autowired
     GreetingRepository repository;
+
     @Override
     public Collection<Greeting> findAll() {
         Collection<Greeting> greetings = repository.findAll();
@@ -29,18 +34,23 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(propagation=Propagation.REQUIRED,readOnly = false)
     public Greeting create(Greeting greeting) {
-        if(greeting.getId()!= null){
+        if (greeting.getId() != null) {
             return null;
         }
         Greeting newGreeting = repository.save(greeting);
+        if(newGreeting.getId() == 4L){
+            throw new RuntimeException("Roll me back!");
+        }
         return newGreeting;
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED,readOnly = false)
     public Greeting update(Greeting greeting) {
         Greeting greetingPersisted = repository.findOne(greeting.getId());
-        if(greetingPersisted == null){
+        if (greetingPersisted == null) {
             return null;
         }
         Greeting updatedGreeting = repository.save(greeting);
@@ -48,6 +58,7 @@ public class GreetingServiceBean implements GreetingService {
     }
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED,readOnly = false)
     public void delete(Long id) {
         repository.delete(id);
     }
