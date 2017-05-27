4f1c36477d38452e2dadbfd4792d4201cbe6f8d0
hojatv <hojat.vaheb@gmail.com>
2017-05-27 18:39:28 +0800

    Profiles and Environment Specific Configs

--- src/main/java/org/learning/abshar/ws/batch/GreetingBatchBeen.java
+++ src/main/java/org/learning/abshar/ws/batch/GreetingBatchBeen.java
@@ -5,11 +5,13 @@ import org.learning.abshar.ws.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
+@Profile("batch")
 @Component
 public class GreetingBatchBeen {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -17,51 +19,51 @@ public class GreetingBatchBeen {
     @Autowired
     private GreetingService greetingService;
 
-    /*@Scheduled(cron = "0,30 * * * * *")*/
-    public void cronJob(){
+    @Scheduled(cron = "${batch.greeting.cron}")
+    public void cronJob() {
         logger.info(" > cronJob");
 
         //Add scheduled Logic here
         Collection<Greeting> greetings = greetingService.findAll();
-        logger.info("There are {} greetings in data store",greetings.size());
+        logger.info("There are {} greetings in data store", greetings.size());
 
         logger.info(" < cronJob");
     }
 
-    /*@Scheduled(initialDelay = 5000, fixedRate = 15000)*/
-    public void fixedRateJobWithInitialDelay(){
+    @Scheduled(initialDelayString = "${batch.greeting.initialDelay}", fixedRateString = "${batch.greeting.fixedRate}")
+    public void fixedRateJobWithInitialDelay() {
         logger.info("> fixedRateJobWithInitialDelay");
 
         //Add scheduled logic here
         //Simulate job processing time
         long pause = 5000;
         long start = System.currentTimeMillis();
-        do{
-            if(start + pause < System.currentTimeMillis()){
+        do {
+            if (start + pause < System.currentTimeMillis()) {
                 break;
             }
-        }while (true);
+        } while (true);
 
-        logger.info("processing time was {} seconds." , pause/1000);
+        logger.info("processing time was {} seconds.", pause / 1000);
 
         logger.info("< fixedRateJobWithInitialDelay");
     }
 
-    /*@Scheduled(initialDelay = 5000 , fixedDelay = 15000)*/
-    public void fixedDelayJobWithInitialDelay(){
+    @Scheduled(initialDelayString = "${batch.greeting.initialDelay}", fixedDelayString = "${batch.greeting.fixedDelay}")
+    public void fixedDelayJobWithInitialDelay() {
         logger.info("> fixedRateJobWithInitialDelay");
 
         //Add scheduled logic here
         //Simulate job processing time
         long pause = 5000;
         long start = System.currentTimeMillis();
-        do{
-            if(start + pause < System.currentTimeMillis()){
+        do {
+            if (start + pause < System.currentTimeMillis()) {
                 break;
             }
-        }while (true);
+        } while (true);
 
-        logger.info("processing time was {} seconds." , pause/1000);
+        logger.info("processing time was {} seconds.", pause / 1000);
 
         logger.info("< fixedRateJobWithInitialDelay");
     }
