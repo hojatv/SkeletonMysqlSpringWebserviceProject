0c7fc41be4b3a003d586cdb2577abb7157e6f2ad
hojatv <hojat.vaheb@gmail.com>
2017-05-28 23:16:12 +0800

    Monitoring (such as count-health, shutdown) using spring boot Actuators

--- src/main/java/org/learning/abshar/ws/service/GreetingServiceBean.java
+++ src/main/java/org/learning/abshar/ws/service/GreetingServiceBean.java
@@ -3,6 +3,7 @@ package org.learning.abshar.ws.service;
 import org.learning.abshar.ws.model.Greeting;
 import org.learning.abshar.ws.repository.GreetingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -20,12 +21,16 @@ import java.util.Map;
         readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
+    @Autowired
+    private CounterService counterService;
+
 
     @Autowired
     GreetingRepository repository;
 
     @Override
     public Collection<Greeting> findAll() {
+        counterService.increment("method.invoked.greetingServiceBean.findAll");
         Collection<Greeting> greetings = repository.findAll();
         return greetings;
     }
@@ -33,6 +38,7 @@ public class GreetingServiceBean implements GreetingService {
     @Override
     @Cacheable(value = "greetings", key = "#id")
     public Greeting findOne(Long id) {
+        counterService.increment("method.invoked.greetingServiceBean.findOne");
         Greeting greeting = repository.findOne(id);
         return greeting;
     }
@@ -41,6 +47,7 @@ public class GreetingServiceBean implements GreetingService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @CachePut(value = "greetings", key = "#result.id")
     public Greeting create(Greeting greeting) {
+        counterService.increment("method.invoked.greetingServiceBean.create");
         if (greeting.getId() != null) {
             return null;
         }
@@ -55,6 +62,7 @@ public class GreetingServiceBean implements GreetingService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @CachePut(value = "greetings", key = "#greeting.id")
     public Greeting update(Greeting greeting) {
+        counterService.increment("method.invoked.greetingServiceBean.update");
         Greeting greetingPersisted = repository.findOne(greeting.getId());
         if (greetingPersisted == null) {
             return null;
@@ -67,12 +75,13 @@ public class GreetingServiceBean implements GreetingService {
     @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @CacheEvict(value = "greetings", key = "#id")
     public void delete(Long id) {
+        counterService.increment("method.invoked.greetingServiceBean.delete");
         repository.delete(id);
     }
 
     @Override
     @CacheEvict(value = "greetings" , allEntries = true)
     public void evictCache(){
-
+        counterService.increment("method.invoked.greetingServiceBean.evictCache");
     }
 }
